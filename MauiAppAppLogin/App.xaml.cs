8c433c315cbe21edc5991bc55911e867d071f31a
Danielle <Danielle@DESKTOP-OC62CTM>
2024-10-19 19:51:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppAppLogin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppAppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
