57fef43d9f0689ab039abbd8edb2b399c6192229
Daniellespaiva <daniellespaiva@outlook.com>
2024-10-21 00:34:08 -0300

    Finalização do projeto login

--- MauiAppAppLogin/App.xaml.cs
+++ MauiAppAppLogin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiAppAppLogin
+﻿
+namespace MauiAppAppLogin
 {
     public partial class App : Application
     {
@@ -6,13 +7,30 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            string? usuario_logado = null;
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+                
+                if (usuario_logado == null) 
+                {
+                    MainPage = new Login();
+
+                }else 
+                {
+                    MainPage = new Protegida();
+                }
+            });
+
+            MainPage = new Login();// Alterando new AppShell e Adicionando tela inicial como new Login
         }
         //ajustando a dimensão da minha tela de login
-        protected override Window CreateWindow(IActivationState activation)
+        protected override Window CreateWindow(IActivationState? activationState)
         {
-            var window = base.CreateWindow(activation);
-
+            var window = base.CreateWindow(activationState);
+        
+        
             window.Width = 400;//largura da tela
             window.Height = 600;//Altura da tela
 
--- MauiAppAppLogin/Login.xaml.cs
+++ MauiAppAppLogin/Login.xaml.cs
@@ -6,4 +6,54 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+
+		try
+		{
+
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>() //Definir a lista de usuario no Array de objeto
+			{
+				new DadosUsuario() // posição 1 daa lista
+				{
+					Usuario = "jose",// Primeiro usuario
+					Senha = "123"
+				},
+				new DadosUsuario() // Posição 2 
+				{
+                    Usuario = "maria",//Usuario 2
+					Senha = "321"
+				}
+			};
+
+			DadosUsuario dados_digitados = new DadosUsuario()// Objeto criado Dados digitados pelo usuario
+			{
+				Usuario = txt_usuario.Text,// dados digitados do Entry
+				Senha = txt_senha.Text // dados digitados do Entry
+			};
+
+			//LINQ
+			//IF  "i=> Para cada" item da lista eu vou verifica se o que foi digitado como usuario e senha exite em algum item da lista
+			if (lista_usuarios.Any(
+				i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha) ))
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+				App.Current.MainPage = new Protegida();
+
+			}
+			else 
+			{
+				throw new Exception("Usuário ou senha incorretos.");
+			}
+		
+
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+
+		}
+    }
 }
